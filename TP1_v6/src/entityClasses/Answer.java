0aa4a9e5c8a30090f5bceef8a1f216b0ad816393
brandonwonng <brandonjwong99@gmail.com>
2025-06-13 19:51:39 -0700

    Update Answer.java

--- TP1_v6/src/entityClasses/Answer.java
+++ TP1_v6/src/entityClasses/Answer.java
@@ -1,46 +1,57 @@
 package entityClasses;
+import entityClasses.PrivateMessage;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Answer{
+
+
+public class Answer {
 	private User 		user;
 	private Question 	question;
 	private String 		textContent;
 	private boolean		acceptance;
-	
+
+	private List<PrivateMessage> privateMessages = new ArrayList<>();
+
 	public Answer() {}
-	
+
 	public void setUser(User name) {
 		user = name;
 	}
-	
+
 	public void setQuestion(Question quest) {
 		question = quest;
 	}
-	
-	
+
 	public void setText(String text) {
 		textContent = text;
 	}
-	
-	
+
 	public void setAcceptance(boolean accept) {
 		acceptance = accept;
 	}
-	
-	
-	//Getter methods
+
 	public User getUser() {
 		return user;
 	}
-	
+
 	public String getText() {
 		return textContent;
 	}
-	
+
 	public Question getQuestion() {
 		return question;
 	}
-	
+
 	public boolean getAcceptance() {
 		return acceptance;
+	}
+
+	public void addPrivateMessage(PrivateMessage pm) {
+		privateMessages.add(pm);
+	}
+
+	public List<PrivateMessage> getPrivateMessages() {
+		return privateMessages;
 	}
 }
