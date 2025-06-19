db849edab39b997e3ffee1024c5ed14b81fd9a99
ClayH224 <clayhauser2@gmail.com>
2025-06-19 16:47:55 -0400

    This is the class for the trusted reviewer page

---
+++ TP1_v6/src/guiPageClasses/GUIStudentTrustedReviewerPage.java
@@ -0,0 +1,244 @@
+package guiPageClasses;
+
+import java.util.Optional;
+
+import applicationMainMethodClasses.FCMainClass;
+import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextInputDialog; 
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import databaseClasses.Database;
+import entityClasses.User;
+import entityClasses.Question;
+import entityClasses.QuestionSet;
+import entityClasses.Answer;
+import entityClasses.AnswerSet;
+import entityClasses.PrivateMessage;
+import javafx.scene.text.*;
+import utilityClasses.UnreadAnswerTracker; // added for tracking unread answers
+
+/*******
+ * <p> Title: GUIStudentQuestionPage Class. </p>
+ * 
+ * <p> Description: The Java/FX-based Student Question Page.</p>
+ * 
+ * <p> Copyright: Clay Hauser © 2025 </p>
+ * 
+ * @author Clay Hauser
+ * 
+ * @version 1.00		2025-04-20 Initial version
+ *  
+ */
+
+public class GUIStudentTrustedReviewerPage{
+	
+	/**********************************************************************************************
+
+	Attributes
+	
+	**********************************************************************************************/
+	
+	// These are the application values required by the user interface
+	private Label label_PageTitle = new Label();
+
+    private Label label_Purpose =  new Label("Manage who your trusted reviewers are."); 
+    
+	private Line line_Separator1 = new Line(20, 95, FCMainClass.WINDOW_WIDTH-20, 95);
+	
+	private Line line_Separator4 = new Line(20, 525, FCMainClass.WINDOW_WIDTH-20,525);
+	
+	private Button button_BackToStudentHomePage = new Button("Return to the Student Home Page");
+	
+	private ScrollPane questionPaneScroll = new ScrollPane();
+
+	private Button button_Logout = new Button("Logout");
+	private Button button_Quit = new Button("Quit");
+
+	private Stage thePrimaryStage;
+	private Pane theRootPane;
+	private Pane questionPane;
+	private Database theDatabase;
+	private User theUser;
+	
+	
+	Optional<String> result;
+
+
+	/**********************************************************************************************
+
+	Constructors
+	
+	**********************************************************************************************/
+
+	
+	/**********
+	 * <p> Method: GUIAdminUpdatePage(Stage ps, Pane theRoot, Database database, User user) </p>
+	 * 
+	 * <p> Description: This method initializes all the elements of the graphical user interface
+	 * This method determines the location, size, font, color, and change and event handlers for
+	 * each GUI object. </p>
+	 * 
+	 * @param ps specifies the JavaFX Stage to be used for this GUI and it's methods
+	 * 
+	 * @param theRoot specifies the JavaFX Pane to be used for this GUI and it's methods
+	 * 
+	 * @param database specifies the Database to be used by this GUI and it's methods
+	 * 
+	 * @param user specifies the User for this GUI and it's methods
+	 * 
+	 */
+	@SuppressWarnings("unused")
+	public GUIStudentTrustedReviewerPage(Stage ps, Pane theRoot, Database database, User user) {
+		thePrimaryStage = ps;
+		theRootPane = theRoot;
+		theDatabase = database;
+		theUser = user;
+		
+		double WINDOW_WIDTH = FCMainClass.WINDOW_WIDTH;
+		
+		//Create a new pane to house the question objects
+		questionPane = new Pane();
+		questionPane.setLayoutX(WINDOW_WIDTH/2 - 100);
+		questionPane.setLayoutY(100);
+		
+		//Create a scrollpane to enable scrolling in the question window
+		questionPaneScroll.setContent(questionPane);
+		questionPaneScroll.setLayoutX(WINDOW_WIDTH/2 - 100);
+		questionPaneScroll.setLayoutY(100);
+		
+		//Lock Window Size to WINDOW_WIDTH/2+100, 360
+		questionPaneScroll.setMaxSize(WINDOW_WIDTH/2+100, 360);
+		questionPaneScroll.setMinSize(WINDOW_WIDTH/2+100, 360);
+		
+		//Set the stage title
+		thePrimaryStage.setTitle("CSE 360 Foundation Code: Student Trusted Reviewer Page");
+		
+		label_PageTitle.setText("Student Trusted Reviewer Page");
+		setupLabelUI(label_PageTitle, "Arial", 28, WINDOW_WIDTH, Pos.CENTER, 0, 5);
+		
+		setupLabelUI(label_Purpose, "Arial", 10, WINDOW_WIDTH, Pos.CENTER, 0, 35);
+		
+		//Set up for all main buttons on the page
+		
+        setupButtonUI(button_BackToStudentHomePage, "Dialog", 18, 300, 
+        		Pos.CENTER, WINDOW_WIDTH/2-150, 475);
+        button_BackToStudentHomePage.setOnAction((event) -> {goToStudentHomePage();});
+		
+        setupButtonUI(button_Logout, "Dialog", 18, 250, Pos.CENTER, 20, 540);
+        button_Logout.setOnAction((event) -> {performLogout(); });
+        
+        setupButtonUI(button_Quit, "Dialog", 18, 250, Pos.CENTER, 300, 540);
+        button_Quit.setOnAction((event) -> {performQuit(); });
+        
+        questionPane.getChildren().clear();
+        
+        // Place all of the just-initialized GUI elements into the pane
+        theRoot.getChildren().clear();
+        theRoot.getChildren().addAll(label_PageTitle,
+        		questionPaneScroll,
+           		label_Purpose,
+        		button_BackToStudentHomePage, 
+        		line_Separator4,
+        		line_Separator1,
+        		button_Logout,
+        		button_Quit
+        		);
+        questionPane.getChildren().clear();
+	}
+	
+	
+	public void setup(User user) {
+		theUser = user; //Clay Edit
+		theRootPane.getChildren().clear();
+		theRootPane.getChildren().addAll(label_PageTitle,
+        		questionPaneScroll,
+           		label_Purpose,
+        		button_BackToStudentHomePage, 
+        		line_Separator4,
+        		line_Separator1,
+        		button_Logout,
+        		button_Quit
+    		);
+		questionPane.getChildren().clear();
+	}	
+	
+	
+	/**********
+	 * Private local method to initialize the standard fields for a label
+	 */
+	
+	private void setupLabelUI(Label l, String ff, double f, double w, Pos p, double x, double y){
+		l.setFont(Font.font(ff, f));
+		l.setMinWidth(w);
+		l.setAlignment(p);
+		l.setLayoutX(x);
+		l.setLayoutY(y);		
+	}
+	
+	
+	/**********
+	 * Private local method to initialize the standard fields for a button
+	 * 
+	 * @param b		The Button object to be initialized
+	 * @param ff	The font to be used
+	 * @param f		The size of the font to be used
+	 * @param w		The width of the Button
+	 * @param p		The alignment (e.g. left, centered, or right)
+	 * @param x		The location from the left edge (x axis)
+	 * @param y		The location from the top (y axis)
+	 */
+	private void setupButtonUI(Button b, String ff, double f, double w, Pos p, double x, double y){
+		b.setFont(Font.font(ff, f));
+		b.setMinWidth(w);
+		b.setAlignment(p);
+		b.setLayoutX(x);
+		b.setLayoutY(y);		
+	}
+
+	/**********
+	 * Private local method to initialize the standard fields for a text field
+	 *
+	private void setupTextUI(TextField t, String ff, double f, double w, Pos p, double x, double y, boolean e){
+		t.setFont(Font.font(ff, f));
+		t.setMinWidth(w);
+		t.setMaxWidth(w);
+		t.setAlignment(p);
+		t.setLayoutX(x);
+		t.setLayoutY(y);		
+		t.setEditable(e);
+	}	
+	
+	/**********************************************************************************************
+
+	User Interface Actions for this page
+	
+	**********************************************************************************************/
+	
+	private void goToStudentHomePage() {
+        // Proceed to the student account update page
+		if (GUISystemStartUpPage.theStudentHomePage == null) {
+			GUISystemStartUpPage.theStudentHomePage = 
+					new GUIStudentHomePage(thePrimaryStage, theRootPane, theDatabase, theUser);
+		}
+		else	
+			GUISystemStartUpPage.theStudentHomePage.setup();
+	}
+	
+	
+	private void performLogout() {
+		GUISystemStartUpPage.theSystemStartupPage.setup();
+	}
+
+	
+	private void performQuit() {
+		System.out.println("Perform Quit");
+		System.exit(0);
+	}
+
+}
