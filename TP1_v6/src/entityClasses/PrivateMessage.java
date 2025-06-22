05ab721fb2bf485e0466dc1b3cde92b3aabdf77c
ClayH224 <clayhauser2@gmail.com>
2025-06-22 00:33:35 -0400

    Update PrivateMessage.java
    Expanded to make it easier to add to the database

--- TP1_v6/src/entityClasses/PrivateMessage.java
+++ TP1_v6/src/entityClasses/PrivateMessage.java
@@ -1,19 +1,45 @@
 package entityClasses;
 
 public class PrivateMessage {
+    private User receiver;
     private User sender;
     private String content;
+    private String parentText;
+    private boolean read;
 
     public PrivateMessage(User sender, String content) {
         this.sender = sender;
         this.content = content;
     }
-
+    
+    public PrivateMessage(User receiver, User sender, String content, String parentText) {
+        this.sender = sender;
+        this.content = content;
+        this.parentText = parentText;
+        this.receiver = receiver;
+        read = false;
+    }
+    
     public User getSender() {
         return sender;
     }
 
     public String getContent() {
         return content;
+    }
+        public String getParentText() {
+    	return parentText;
+    }
+    
+    public User getReceiver() {
+    	return receiver;
+    }
+    
+    public boolean getRead() {
+    	return read;
+    }
+    
+    public void setRead(boolean status) {
+    	read = status;
     }
 }
