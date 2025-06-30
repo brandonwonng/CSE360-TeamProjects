f83ec5cf94f137253fa25c0683401e0eb7515efb
ClayH224 <clayhauser2@gmail.com>
2025-06-30 16:28:45 -0400

    Update GUIFacultyDashPage.java
    made some changes for the work requests

--- TP1_v6/src/guiPageClasses/GUIFacultyDashPage.java
+++ TP1_v6/src/guiPageClasses/GUIFacultyDashPage.java
@@ -22,6 +22,7 @@ import entityClasses.ReviewerTrust;
 import entityClasses.Answer;
 import entityClasses.AnswerSet;
 import entityClasses.PrivateMessage;
+import entityClasses.WorkRequest;
 import javafx.scene.text.*;
 import utilityClasses.UnreadAnswerTracker; // added for tracking unread answers
 import java.util.ArrayList;
@@ -52,7 +53,9 @@ public class GUIFacultyDashPage {
 	private Label label_UserDetails = new Label();
 	
 	private Button button_MessageDashboard = new Button("See Faculty Messages");
+	private Button button_WorkRequestDashboard = new Button("See WorkRequests");
 	private Button button_SubmitMessage = new Button("Message Faculty");
+	Button button_createNewWorkRequest = new Button("Create New Work Request");
 	private Button button_GoHome = new Button("Return Home");
 	
 	private Line line_Separator1 = new Line(20, 95, FCMainClass.WINDOW_WIDTH-20, 95);
@@ -132,9 +135,29 @@ public class GUIFacultyDashPage {
 		label_UserDetails.setText("User: " + user.getUserName());
 		setupLabelUI(label_UserDetails, "Arial", 20, WINDOW_WIDTH, Pos.BASELINE_LEFT, 20, 55);
 		
+		if(theUser.getInstructorRole()) {
+			
+			setupButtonUI(button_createNewWorkRequest, "Dialog", 18, 250, Pos.CENTER, 20, 290);
+			
+			TextInputDialog dialogNewWork = new TextInputDialog();
+			dialogNewWork.setTitle("Enter your request");
+			dialogNewWork.setHeaderText("Enter your request");
+			
+			button_createNewWorkRequest.setOnAction((event) -> {result = dialogNewWork.showAndWait();
+			if(result.isPresent()) {
+				theDatabase.createWorkRequest(theUser.getUserName(), result.get());
+				dialogNewWork.close();
+			}
+			});
+			
+		}
 		
 		setupButtonUI(button_MessageDashboard, "Dialog", 18, 250, Pos.CENTER, 20, 150);
 		button_MessageDashboard.setOnAction((event) -> {populateMessages(); });
+		
+		setupButtonUI(button_WorkRequestDashboard, "Dialog", 18, 250, Pos.CENTER, 20, 220);
+		button_WorkRequestDashboard.setOnAction((event) -> {populateRequests();
+		showRequests(theDatabase.getAllRequests());});
 		
 		setupButtonUI(button_GoHome, "Dialog", 18, 250, Pos.CENTER, 20, 470);
 		button_GoHome.setOnAction((event) -> {goToUserHomePage(); });
@@ -166,9 +189,13 @@ public class GUIFacultyDashPage {
 	        messagePaneScroll,
 	        button_GoHome,
 	        button_MessageDashboard,
+	        button_WorkRequestDashboard,
 	        button_Logout,
 	        button_Quit
 	    );
+	    if(theUser.getInstructorRole()) {
+	    	theRootPane.getChildren().add(button_createNewWorkRequest);
+	    }
 	}
 	
 	/**********
@@ -218,12 +245,20 @@ public class GUIFacultyDashPage {
 	            GUISystemStartUpPage.theInstructorHomePage.setup();
 	        }
 		}
-		else {		
+		else if (theUser.getStaffRole()){		
 		    if (GUISystemStartUpPage.theStaffHomePage == null) {
 	            GUISystemStartUpPage.theStaffHomePage =
 	                new GUIStaffHomePage(primaryStage, theRootPane, theDatabase, theUser);
 	        } else {
 	            GUISystemStartUpPage.theStaffHomePage.setup();
+	        }
+		}
+		else {
+			if (GUISystemStartUpPage.theAdminHomePage == null) {
+	            GUISystemStartUpPage.theAdminHomePage =
+	                new GUIAdminHomePage(primaryStage, theRootPane, theDatabase, theUser);
+	        } else {
+	            GUISystemStartUpPage.theAdminHomePage.setup();
 	        }
 		}
 	}
@@ -256,6 +291,108 @@ public class GUIFacultyDashPage {
     		text.setLayoutY(60 + (i * 45));
     		messagePane.getChildren().add(text);
 		}
+	}
+	
+	private void populateRequests() {
+		messagePane.getChildren().clear();
+		
+		ArrayList<WorkRequest> requests; 
+		
+		TextInputDialog dialogRequestUpdate = new TextInputDialog();
+		dialogRequestUpdate.setTitle("Enter your update to the request");
+		dialogRequestUpdate.setHeaderText("Enter your update to the request");
+		
+		Button button_SeeAllRequests = new Button("See All Requests");
+		Button button_SeeOpenRequests = new Button("See Open Requests");
+		Button button_SeeClosedRequests = new Button("See Closed Requests");
+
+		setupButtonUI(button_SeeAllRequests, "Dialog", 15, 120, Pos.CENTER, 0, 0);
+		button_SeeAllRequests.setOnAction((event) -> {showRequests(theDatabase.getAllRequests());});
+		
+		setupButtonUI(button_SeeOpenRequests, "Dialog", 15, 120, Pos.CENTER, 150, 0);
+		button_SeeOpenRequests.setOnAction((event) -> {showRequests(theDatabase.getAllOpenRequests());});
+		
+		setupButtonUI(button_SeeClosedRequests, "Dialog", 15, 120, Pos.CENTER, 320, 0);
+		button_SeeClosedRequests.setOnAction((event) -> {showRequests(theDatabase.getAllClosedRequests());});
+		
+		messagePane.getChildren().addAll(button_SeeAllRequests,button_SeeOpenRequests,button_SeeClosedRequests);
+	}
+	
+	private void showRequests(ArrayList<WorkRequest> requests) {
+		messagePane.getChildren().clear();
+		populateRequests();
+		
+		Button seeComments;
+		Button closeRequest;
+		Button reopenRequest;
+		WorkRequest request;
+		Text text;
+		for(int i = 0; i < requests.size()*2; i+=2) {
+			request = requests.get(i/2);
+			text = new Text(String.format("%s : %s",request.getOriginator().getUserName(), request.getDescription()));
+			text.setLayoutY(60 + i*30);
+			
+			seeComments = new Button("See Comments");
+			setupButtonUI(seeComments, "Dialog", 12, 170, Pos.CENTER, 0, 45 + (i+1)*30);
+			seeComments.setOnAction((event) -> { Button but = (Button) event.getSource();
+				int index = (((int)but.getLayoutY() - 45) / 60);
+				viewComments(requests.get(index));
+				});
+			
+			if(request.getClosedStatus() && theUser.getInstructorRole()) {
+				reopenRequest = new Button("Reopen Request");
+				
+				setupButtonUI(reopenRequest, "Dialog", 12, 170, Pos.CENTER, 175, 45 + (i+1)*30);
+				reopenRequest.setOnAction((event) -> { Button but = (Button) event.getSource();
+					int index = ((int)but.getLayoutY() - 45) / 60;
+					theDatabase.reopenWorkRequest(requests.get(index).getDescription());
+					but.setDisable(true);});
+				messagePane.getChildren().add(reopenRequest);
+			}
+			else if(!request.getClosedStatus() && theUser.getAdminRole()){
+				closeRequest = new Button("Close Request");
+				
+				setupButtonUI(closeRequest, "Dialog", 12, 170, Pos.CENTER, 175, 45 + (i+1)*30);
+				closeRequest.setOnAction((event) -> {Button but = (Button) event.getSource();
+				int index = ((int)but.getLayoutY() - 45) / 60;
+				theDatabase.closeWorkRequest(requests.get(index).getDescription());
+				but.setDisable(true);});
+				
+				messagePane.getChildren().add(closeRequest);
+			}
+			messagePane.getChildren().addAll(text, seeComments);
+			
+		}
+	}
+	
+	private void viewComments(WorkRequest request) {
+		messagePane.getChildren().clear();
+		ArrayList<PrivateMessage> comments = theDatabase.getComments(request.getDescription());
+		Button submitUpdate;
+		PrivateMessage comment;
+		Text text;
+		
+		TextInputDialog dialogSendMessage = new TextInputDialog();
+		dialogSendMessage.setTitle("Enter your message");
+		dialogSendMessage.setHeaderText("Enter your message");
+		
+		submitUpdate = new Button("Submit an Update");
+		setupButtonUI(submitUpdate, "Dialog", 12, 170, Pos.CENTER, 0, 0);
+		submitUpdate.setOnAction((event) -> {result = dialogSendMessage.showAndWait();
+		if(result.isPresent()) {
+				theDatabase.addRequestComment(theUser, request.getDescription(), result.get());
+				viewComments(request);
+			}
+		});
+		messagePane.getChildren().add(submitUpdate);
+		
+		for(int i = 0; i < comments.size(); i++) {
+			comment = comments.get(i);
+			text = new Text(String.format("Update from %s: %s", comment.getSender().getUserName(), comment.getContent()));
+			text.setLayoutY((i *45)+45);
+			messagePane.getChildren().add(text);
+		}
+		
 	}
 	
 	private void performLogout() {
